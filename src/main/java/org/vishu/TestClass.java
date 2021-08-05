657427f8e4ca8255bd0d74e2a28f32f5d4e0200c
Vishu Gupta <Vishu.Gupta@go-mmt.com>
2021-08-05 15:06:24 +0530

    removing callVishu

--- src/main/java/org/vishu/TestClass.java
+++ src/main/java/org/vishu/TestClass.java
@@ -2,9 +2,10 @@ package org.vishu;
 
 public class TestClass {
 
-    public static void callVishu() {
-
-    }
+    // Removing for version 3.0
+//    public static void callVishu() {
+//
+//    }
 
     public static void callVishu2() {
 
